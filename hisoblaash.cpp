c70370228bda99d40753f57d1d6f98ba8ac28560
baxtiyorovshodiyor987-debug <baxtiyorovshodiyor987@gmail.com>
2026-05-04 22:27:35 +0500

    Update hisoblaash.cpp

--- hisoblaash.cpp
+++ hisoblaash.cpp
@@ -1,1 +1,40 @@
+using namespace std;
 
+// Abstraksiya + Inkapsulyatsiya
+class Person {
+private:
+    string name;
+
+public:
+    Person(string n) {
+        name = n;
+    }
+
+    void setName(string n) {
+        name = n;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    // Polimorfizm uchun virtual funksiya
+    virtual void introduce() {
+        cout << "Men insonman: " << name << endl;
+    }
+};
+
+// Merosxo‘rlik
+class Student : public Person {
+public:
+    Student(string n) : Person(n) {}
+
+    void introduce() override {
+        cout << "Men talabaman: " << getName() << endl;
+    }
+};
+
+int main() {
+    Student s("Ali");
+    s.introduce();
+}
